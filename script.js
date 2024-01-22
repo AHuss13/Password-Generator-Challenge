0524d595d7b5e5e0b9389026764983533f031d01
Adam Huss <adamhuss13@gmail.com>
2024-01-22 14:31:38 -0600

    added confirms for characters

--- script.js
+++ script.js
@@ -12,26 +12,30 @@ function writePassword() {
 function generatePassword() {
   //---prompt for password criteria---
   //---choose 8-12 characters---
-  var passwordLength = prompt("Please choose a password length between 8 and 128 characters")
+  var passwordLength = prompt("Please choose a password length between 8 and 128 characters.")
 
   if (passwordLength < 8 || passwordLength > 128) {
-    alert ("Please input a valid length");
+    alert ("Please input a valid length.");
     return;
   };
 
   //---check for uppercase choice---
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+  confirm("If you would like to include uppercase letters, choose OK");
 
 
   //---check for lowercase choice---
   var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  confirm("If you would like to include lowercase letters, choose OK");
 
   //---check for numeric choice---
   var numberChoice = "0123456789"
+  confirm("If you would like to include numbers, choose OK");
 
   //---check for specials choice---
   var specialChar = "!@#$%^&*()_+-=[]\{}|;':,./<>?"
+  confirm("If you would like to include special characters, choose OK");
 
   //---generate password---
 
