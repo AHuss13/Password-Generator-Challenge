70431381fed71926021a0659feca2052e7cc5152
Adam Huss <adamhuss13@gmail.com>
2024-01-23 15:28:54 -0600

    moved checks back into generate password, added password vars, and fixed choice length

--- script.js
+++ script.js
@@ -8,42 +8,46 @@ function writePassword() {
 
   passwordText.value = password;
 }
+function generatePassword() {
 
 //---prompt for password criteria---
-//---choose 8-12 characters---
-var passwordLength = prompt("Please choose a password length between 8 and 128 characters.")
+  //---choose 8-12 characters---
+  var passwordLength = prompt("Please choose a password length between 8 and 128 characters.")
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Please input a valid length.");
+    return;
+  };
+  
+  //---check for uppercase choice---
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var addUpper= confirm("If you would like to include uppercase letters, choose OK");
+  
+  //---check for lowercase choice---
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var addLower = confirm("If you would like to include lowercase letters, choose OK");
+  
+  //---check for numeric choice---
+  var numberChoice = "0123456789"
+  var addNumber = confirm("If you would like to include numbers, choose OK");
+  
+  //---check for specials choice---
+  var specialChar = "!@#$%^&*()_+-=[]\{}|;':,./<>?"
+  var addSpecial = confirm("If you would like to include special characters, choose OK");
 
-if (passwordLength < 8 || passwordLength > 128) {
-  alert ("Please input a valid length.");
-  return;
-};
 
-//---check for uppercase choice---
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var addUpper= confirm("If you would like to include uppercase letters, choose OK");
-
-//---check for lowercase choice---
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
-var addLower = confirm("If you would like to include lowercase letters, choose OK");
-
-//---check for numeric choice---
-var numberChoice = "0123456789"
-var addNumber = confirm("If you would like to include numbers, choose OK");
-
-//---check for specials choice---
-var specialChar = "!@#$%^&*()_+-=[]\{}|;':,./<>?"
-var addSpecial = confirm("If you would like to include special characters, choose OK");
-
-function generatePassword() {
   //---generate password---
-  for (var i = 0; i < passwordLength; i++)
-  var blankPassword = ""
-  if (addUpper) {blankPassword += upperCase}
-  if (addLower) {blankPassword += lowerCase}
-  if (addNumber) {blankPassword += numberChoice}
-  if (addSpecial) {blankPassword += specialChar}
-
-
+  var possibleChars = "";
+  var newPassword = "";
+  if (addUpper) {possibleChars += upperCase};
+  if (addLower) {possibleChars += lowerCase};
+  if (addNumber) {possibleChars += numberChoice};
+  if (addSpecial) {possibleChars += specialChar};
+  
+  for (var i = 0; i < passwordLength; i++) {
+    newPassword += possibleChars[Math.floor(Math.random() * possibleChars.length)]
+  }
+  return newPassword;
   //filler to confirm results while working
   //return "You have a test result!";
 };
